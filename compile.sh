3fd83a29bb3a5ccea235a751a902a3bf35be28ba
David Blackman <david@blackmad.com>
2019-11-04 09:43:11 -0500

    initial assemble import

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -f tweego ]; then
-  echo 'must download tweego binary to ./tweego'
-  exit 1
-fi
-
-rm out.html
-./tweego -f sugarcube-2 example.tw ../../audioonend.js > out.html
